f8ea0ea6717b274d54865e83766ce31864ff0d7f
ivan-sygin <ivan.sygin@yandex.ru>
2023-12-01 16:03:38 +0300

    my work 01.12.2023

--- Genshin/src/pages/MainPage.jsx
+++ Genshin/src/pages/MainPage.jsx
@@ -2,8 +2,29 @@ import { FaUser, FaPlus, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 import Button from '@mui/material/Button'
 import './css/colors.css'
 import './css/MainPage.css'
+import { useState } from 'react'
+import {ChangeColor} from '../components/colors'
+import { useEffect } from 'react'
+
+class Layla{
+  name = "Лайла"
+  description = "Лайла — Крио саппорт, предоставляет отряду защиту."
+  img="https://genshinpedia.ru/wp-content/uploads/2023/04/1680785407_e67792d0779f602044795c14c525dc57-2048x1199.png"
+  color="Layla"
+}
+class Diluc{
+  name = "Дилюк"
+  description = "Дилюк — Пиро дамагер, предоставляет отряду урон."
+  img="https://i.playground.ru/i/pix/3089903/image.jpg"
+  color="Diluc"
+}
 
 const MainPage = () => {
+  const [character, setCharacter] = useState("Layla");
+  const characters = {"Layla":new Layla(),'Diluc':new Diluc()}
+  useEffect(()=>{
+    ChangeColor('Layla');
+  },[])
   return (
     <>
       <div className='headerMainPage'>
@@ -26,29 +47,32 @@ const MainPage = () => {
         </div>
       </div>
       <div className='bannerMainPage'>
-        <div className='arrowLeftMainPage'>
+        <div className='arrowLeftMainPage'  onClick={() => { setCharacter("Layla"); ChangeColor('Layla'); }}>
           <FaArrowLeft />
         </div>
-        <div className='bannerContainerMainPage'>
-          <div className='leftSideBannerMainPage'>
-            <div className='headerLeftSideContainerPage'>Лайла</div>
+        <div className={'bannerContainerMainPage '+character+"bannerContainerMainPage"}>
+          <div className='leftSideBannerMainPage '>
+            <div className='headerLeftSideContainerPage'>{characters[character].name}</div>
             <div className='descriptionLeftSideContainerPage'>
-              Лайла — Крио саппорт, предоставляет отряду защиту.
+              {characters[character].description}
             </div>
           </div>
           <div className='rightSideBannerMainPage'>
-            <img
-              src='https://genshinpedia.ru/wp-content/uploads/2023/04/1680785407_e67792d0779f602044795c14c525dc57-2048x1199.png'
-              alt=''
-            />
+            <div className="imageInBannerMainPage">
+              <img
+                src={characters[character].img}
+                alt=''
+              />
+            </div>
           </div>
         </div>
-        <div className='arrowRightMainPage'>
+        <div className='arrowRightMainPage' onClick={() => { setCharacter("Diluc"); ChangeColor('Diluc'); }}>
           <FaArrowRight />
         </div>
       </div>
+
     </>
   )
 }
 
-export default MainPage
+export default MainPage;
