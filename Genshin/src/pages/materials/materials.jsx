bda4535bbef2cb12db06af2d1abbf0ceef683d9c
ivan-sygin <ivan.sygin@yandex.ru>
2023-12-09 01:08:29 +0300

    dead inside v3

--- Genshin/src/pages/materials/materials.jsx
+++ Genshin/src/pages/materials/materials.jsx
@@ -6,6 +6,7 @@ import { FaArrowRight } from 'react-icons/fa'
 import { useNavigate, useParams } from 'react-router'
 import { useEffect, useState } from 'react'
 import { ServerAdress2 } from '../../components/ApiVavilin'
+import { TOKEN } from '../../components/TokenController'
 
 const LectionHeader = ({ title }) => {
   return (
@@ -30,11 +31,11 @@ const LectionBody = ({ text }) => {
 const CommentBlock = ({ comment }) => {
   return (
     <Box
-      minHeight={100}
+      minHeight={50}
       display={'flex'}
       flexDirection={'row'}
       alignItems={'start'}
-      border={1}
+      //border={1}
       padding={2}
       borderRadius={'10px'}
       borderColor={'var(--banner-color1)'}
@@ -46,7 +47,7 @@ const CommentBlock = ({ comment }) => {
         overflow={'hidden'}
         marginRight={3}
       >
-        <img src={comment.owner_photo} alt='' width={'100%'} height={'auto'} />
+        <img src={comment?.owner_photo} alt='' width={'100%'} height={'auto'} />
       </Box>
       <Box>
         <Box
@@ -56,12 +57,12 @@ const CommentBlock = ({ comment }) => {
           alignItems={'center'}
           gap={'20px'}
         >
-          <Box fontFamily={'Inter'}>{comment.owner_name}</Box>
+          <Box fontFamily={'Inter'}>{comment?.owner_name}</Box>
           <Box fontFamily={'Inter'} fontSize={'10px'}>
-            {comment.time}
+            {comment?.time}
           </Box>
         </Box>
-        <Box>{comment.comment_text}</Box>
+        <Box>{comment.message}</Box>
       </Box>
     </Box>
   )
@@ -79,9 +80,9 @@ const LectionComments = ({ comments }) => {
           minHeight={200}
           display={'flex'}
           flexDirection={'column'}
-          gap={'20px'}
+          gap={'5px'}
         >
-          {comments.map((_) => {
+          {comments?.map((_) => {
             return <CommentBlock comment={_} />
           })}
         </Box>
@@ -90,13 +91,34 @@ const LectionComments = ({ comments }) => {
   )
 }
 
-const WriteCommentSection = () => {
+const handleClick = (id, fetchData) => {
+  const json = {
+    topic_id: +id,
+    message: document.getElementById('textarea_comment').value
+  }
+  fetch(ServerAdress2 + '/topics/sendComment', {
+    method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + TOKEN,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(json)
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      fetchData(id)
+      console.log(json)
+    })
+}
+
+const WriteCommentSection = ({ id, fetchData }) => {
   let navigate = useNavigate()
   return (
     <Box paddingBottom={'50px'} display={'flex'} flexDirection={'row'}>
       <Box
         component={'textarea'}
         type='text'
+        id='textarea_comment'
         padding={2}
         borderRadius={'20px 0 0 20px'}
         minHeight={'100px'}
@@ -112,7 +134,9 @@ const WriteCommentSection = () => {
         border={0}
         sx={{ cursor: 'pointer' }}
         onClick={() => {
-          window.location.reload()
+          handleClick(id, fetchData)
+
+          //window.location.reload()
         }}
       >
         <FaArrowRight size={'40px'} />
@@ -129,7 +153,7 @@ export const MaterialPage = () => {
     fetch(ServerAdress2 + '/topics/getById?id=' + id)
       .then((response) => response.json())
       .then((json) => {
-        setData(json.topic)
+        setData(json.topic[0])
       })
   }
 
@@ -141,8 +165,8 @@ export const MaterialPage = () => {
       <HeaderMainPage />
       <LectionHeader title={data?.title} />
       <LectionBody text={data?.text} />
-      <LectionComments comments={LectionComments1.comments} />
-      <WriteCommentSection />
+      <LectionComments comments={data?.comments} />
+      <WriteCommentSection id={id_page} fetchData={fetchData} />
     </Box>
   )
 }
